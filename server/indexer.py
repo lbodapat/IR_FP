6e1142b4917ac44dbe69fdecd32b5db15fa78eb2
Ubuntu <ubuntu@ip-172-31-7-80.us-east-2.compute.internal>
2021-12-07 09:15:33 +0000

    s

--- server/indexer.py
+++ server/indexer.py
@@ -189,9 +189,9 @@ class Indexer:
 
 if __name__ == "__main__":
     print("Indexer")
-    # i = Indexer()
-    # i.do_initial_setup()
-    # i.add_fields()
+    #i = Indexer()
+    #i.do_initial_setup()
+    #i.add_fields()
     # i = Indexer()
     # i.do_initial_setup()
     # i.add_fields()
